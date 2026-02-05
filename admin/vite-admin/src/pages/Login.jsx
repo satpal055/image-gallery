8196fffc2295c9d50256a86ed0028eb9e7915186
satpal055 <satpal055singh@gmail.com>
2026-02-05 16:31:00 +0530

    .env api url updated

--- admin/vite-admin/src/pages/Login.jsx
+++ admin/vite-admin/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = import.meta.env.VITE_BASE_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function Login() {
     const [email, setEmail] = useState("");
